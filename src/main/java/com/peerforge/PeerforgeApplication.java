580cab7f97de81853f9caeb863bc31576eff0e60
Dushyant Verma <Dushyantverma.cs11@gmail.com>
2026-06-14 18:43:13 +0530

    Initial PeerForge project setup

---
+++ src/main/java/com/peerforge/PeerforgeApplication.java
@@ -0,0 +1,13 @@
+package com.peerforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerforgeApplication.class, args);
+	}
+
+}
