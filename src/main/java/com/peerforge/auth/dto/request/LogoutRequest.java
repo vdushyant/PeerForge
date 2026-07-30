523f9dec6726dcd36fefc20c1e7e5a833040854d
Dushyant Verma <Dushyantverma.cs11@gmail.com>
2026-07-30 21:03:15 +0530

    Added logout Request DTO

---
+++ src/main/java/com/peerforge/auth/dto/request/LogoutRequest.java
@@ -0,0 +1,11 @@
+package com.peerforge.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LogoutRequest(
+
+        @NotBlank(message = "Refresh token is required")
+        String refreshToken
+
+) {
+}
